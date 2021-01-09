641dd34068f163765d548c889333a55be9318186
talcome <talkome@gmail.com>
2021-01-09 13:58:37 +0200

    add menu and logout button

--- app/src/main/java/com/example/arielcast/StudentActivity.java
+++ app/src/main/java/com/example/arielcast/StudentActivity.java
@@ -10,6 +10,9 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -19,6 +22,7 @@ import android.widget.Toast;
 import com.example.arielcast.firebase.model.dataObject.Course;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -37,8 +41,6 @@ public class StudentActivity extends AppCompatActivity {
     FirebaseRecyclerAdapter<Course,MyViewHolder> adapter;
     DatabaseReference DataRef;
     ArrayList<Course> courses ;
-    String email,id;
-    String UserID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,19 +51,75 @@ public class StudentActivity extends AppCompatActivity {
         studentListView = findViewById(R.id.recycleView);
         studentListView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
         studentListView.setHasFixedSize(true);
-        // get student's email from MainActivity
-        Intent intent = getIntent();
-        email= intent.getExtras().getString("Email");
-        id=intent.getExtras().getString("ID");
 
         DataRef = FirebaseDatabase.getInstance().getReference().child("Courses");
 
-        UserID=id;
-        myAdapter = new MyAdapter(this, getMyList(),UserID);
+        myAdapter = new MyAdapter(this, getMyList());
         studentListView.setAdapter(myAdapter);
 
+
+        // get student's email from MainActivity
+        Intent intent = getIntent();
+        String email= intent.getExtras().getString("Email");
+
+        // LoadData();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.logout: {
+                logOut();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logOut() {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(StudentActivity.this, LoginActivity.class));
+    }
+
+    private void LoadData() {
+       options = new FirebaseRecyclerOptions.Builder<Course>().
+               setQuery(DataRef,Course.class).build();
+
+
+        adapter = new FirebaseRecyclerAdapter<Course, MyViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull MyViewHolder holder, int position, @NonNull Course model) {
+                holder.textView.setText(model.getCourseName());
+                holder.imageView.setImageURI(Uri.parse(model.getImage()));
+                holder.view.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent=new Intent(getApplicationContext(),ShowCourse.class);
+                        intent.putExtra("CourseId",model.getCourseId());
+                        startActivity(intent);
+                    }
+                });
+
+
+            }
+
+
+            @NonNull
+            @Override
+            public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).
+                        inflate(R.layout.single_course,parent,false);
+                return new MyViewHolder(view);
+            }
+        };
+    }
 
     private ArrayList<Course> getMyList(){
         courses = new ArrayList<>();
