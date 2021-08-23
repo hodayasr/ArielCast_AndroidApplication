65088e9df4c11d2d4da53fbfdca196c1105e8300
hodayasr <hodaya_s@hotmail.com>
2021-08-23 22:43:56 +0300

    fullname in comments list and count comments for lecture

--- app/src/main/java/com/example/arielcast/CommentHolder.java
+++ app/src/main/java/com/example/arielcast/CommentHolder.java
@@ -1,6 +1,7 @@
 package com.example.arielcast;
 
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -9,12 +10,14 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class CommentHolder extends RecyclerView.ViewHolder {
     TextView mTitle, mDes , mDate;
+    ImageButton mdel;
 
     public CommentHolder(@NonNull View itemView) {
         super(itemView);
         this.mTitle = itemView.findViewById(R.id.textViewMain);
         this.mDate=itemView.findViewById(R.id.textViewdate);
         this.mDes = itemView.findViewById(R.id.textViewSub);
+        this.mdel=itemView.findViewById(R.id.deletecommentbtn);
 
     }
 
